aed8cef455965aa5e8bd08ef4dbc3708de3bec9a
arjunkumar811 <kumararjun26401@gmail.com>
2025-09-29 21:51:38 +0530

    Added Adapter

--- 1-token-launchpad-starter/src/components/TokenLaunchpad.jsx
+++ 1-token-launchpad-starter/src/components/TokenLaunchpad.jsx
@@ -1,5 +1,18 @@
 
 export function TokenLaunchpad() {
+
+
+    function createToken() {
+        const name = document.getElementById('name').value;
+        const symbol = document.getElementById('symbol').value;
+        const image = document.getElementById('image').value;
+        const supply = document.getElementById('supply').value;  
+
+    
+    }
+
+
+
     return  <div style={{
         height: '100vh',
         display: 'flex',
@@ -8,10 +21,10 @@ export function TokenLaunchpad() {
         flexDirection: 'column'
     }}>
         <h1>Solana Token Launchpad</h1>
-        <input className='inputText' type='text' placeholder='Name'></input> <br />
-        <input className='inputText' type='text' placeholder='Symbol'></input> <br />
-        <input className='inputText' type='text' placeholder='Image URL'></input> <br />
-        <input className='inputText' type='text' placeholder='Initial Supply'></input> <br />
-        <button className='btn'>Create a token</button>
+        <input id="name" className='inputText' type='text' placeholder='Name'></input> <br />
+        <input id="symbol" className='inputText' type='text' placeholder='Symbol'></input> <br />
+        <input id="image" className='inputText' type='text' placeholder='Image URL'></input> <br />
+        <input id="supply" className='inputText' type='text' placeholder='Initial Supply'></input> <br />
+        <button onClick={createToken} className='btn'>Create a token</button>
     </div>
 }
