3bae470bcec1e088829e52838cd060c7f96132bd
arjunkumar811 <kumararjun26401@gmail.com>
2025-10-08 18:23:51 +0530

    Transaction

--- 1-token-launchpad-starter/src/components/TokenLaunchpad.jsx
+++ 1-token-launchpad-starter/src/components/TokenLaunchpad.jsx
@@ -1,14 +1,32 @@
+import { useConnection, useWallet } from "@solana/wallet-adapter-react";
+import { Keypair, SystemProgram, Transaction } from "@solana/web3.js";
+import { MINT_SIZE, TOKEN_2022_PROGRAM_ID } from "@solana/spl-token";
 
 export function TokenLaunchpad() {
+    const wallet = useWallet();
+        const connection = useConnection();
 
+     // const name = document.getElementById('name').value;
+        // const symbol = document.getElementById('symbol').value;
+        // const image = document.getElementById('image').value;
+        // const supply = document.getElementById('supply').value;  
 
-    function createToken() {
-        const name = document.getElementById('name').value;
-        const symbol = document.getElementById('symbol').value;
-        const image = document.getElementById('image').value;
-        const supply = document.getElementById('supply').value;  
+   async function createToken() {
+        const mintKeypair = Keypair.generate();
+        const lamports = await getMinimumBalanceForRentExemptMint(connection);
+        
 
-    
+        const transaction = new Transaction().add(
+            SystemProgram.createAccount({
+                fromPubkey: wallet.publicKey,
+                newAccountPubkey: mintKeypair.publicKey,
+                space: MINT_SIZE,
+                lamports,
+                programId: TOKEN_2022_PROGRAM_ID
+
+            })
+        )        
+       
     }
 
 
@@ -20,7 +38,7 @@ export function TokenLaunchpad() {
         alignItems: 'center',
         flexDirection: 'column'
     }}>
-        <h1>Solana Token Launchpad</h1>
+                <h1>Solana Token Launchpad</h1>
         <input id="name" className='inputText' type='text' placeholder='Name'></input> <br />
         <input id="symbol" className='inputText' type='text' placeholder='Symbol'></input> <br />
         <input id="image" className='inputText' type='text' placeholder='Image URL'></input> <br />
